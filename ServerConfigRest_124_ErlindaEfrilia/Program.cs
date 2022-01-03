18f906c67898c81de976c494c38ab3acfc93e2c9
Erlindaefrilia <erlindaefrilia@gmail.com>
2022-01-04 01:12:59 +0700

    Add project files.

---
+++ ServerConfigRest_124_ErlindaEfrilia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceRest_124_ErlindaEfrilia;
+
+namespace ServerConfigRest_124_ErlindaEfrilia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("SERVER sudahh READYYY");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
